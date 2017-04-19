9623d32df64b8442201856ab7cd9d325be722990
Julie A. Kohler <julie.akohler@gmail.com>
2017-04-18 21:40:55 -0600

    Update WhereArtThou.js

--- Toy_Problems/WhereArtThou.js
+++ Toy_Problems/WhereArtThou.js
@@ -2,7 +2,6 @@ function whereAreYou(collection, source) {
   
   collection.forEach(function(el) {
     for (var prop in el) {
-      console.log("Here is prop", prop, "Here is el", el[prop]);
       if (source[prop] === el[prop]) {
         arr.push(el);
       }
