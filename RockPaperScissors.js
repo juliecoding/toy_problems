99921f6808d7300865506d79b2e1d9ae5bed2b3a
juliecoding <julie.akohler@gmail.com>
2017-05-13 20:46:59 -0600

    Racing Te to be the best Codewarrior

---
+++ RockPaperScissors.js
@@ -0,0 +1,45 @@
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples:
+// rps('scissors','paper') // Player 1 won!
+// rps('scissors','rock') // Player 2 won!
+// rps('paper','paper') // Draw!
+
+const rps = (p1, p2) => {
+  switch (true) {
+    case (p1 === p2):
+      return "Draw!";
+    case (p1 === "rock" && p2 === "scissors"):
+      return "Player 1 won!";
+    case (p1 === "scissors" && p2 === "rock"):
+      return "Player 2 won!";
+    case (p1 === "scissors" && p2 === "paper"):
+      return "Player 1 won!";
+    case (p1 === "paper" && p2 === "scissors"):
+      return "Player 2 won!";
+    case (p1 === "paper" && p2 === "rock"):
+      return "Player 1 won!";
+    case (p1 === "rock" && p2 === "paper"):
+      return "Player 2 won!";
+  }
+};
+
+//BECOMES
+const rps = (p1, p2) => {
+  if (p1 === p2) {
+    return 'Draw!'
+  };
+  return `Player ${/rockscissors|scissorspaper|paperrock/.test(p1+p2)? 1 : 2} won!`;
+}
+
+//OR
+const rps = (p1, p2) => {
+  if (p1 === p2) return "Draw!";
+  var rules = { rock: "scissors", paper: "rock", scissors: "paper" };
+  if (p2 === rules[p1]) {
+    return "Player 1 won!";
+  } else {
+    return "Player 2 won!";
+  }
+};
