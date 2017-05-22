8929c5cdfc7d16cdc2ec0890842476dc2ec1af0f
juliecoding <julie.akohler@gmail.com>
2017-05-22 11:15:17 -0600

    Little morning Codewars exercise

---
+++ EnumerableMagic1.js
@@ -0,0 +1,4 @@
+//Create a method all which takes an array and a predicate (function pointer), and returns true if the predicate returns true for every element in the array. Otherwise, it should return false. If the array is empty, it should return true, since technically nothing failed the test.
+
+
+const all = (arr, fun) => arr.every(fun);
