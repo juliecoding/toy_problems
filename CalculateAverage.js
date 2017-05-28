5d55015d2a2abf5e7781fe0a63a7506620bbac62
juliecoding <julie.akohler@gmail.com>
2017-05-27 23:19:40 -0600

    Codewars

---
+++ CalculateAverage.js
@@ -0,0 +1,6 @@
+//Write function avg which calculates average of numbers in given list.
+
+
+const getTotal = (a, b) => a + b;
+
+const find_average = array => array.reduce(getTotal) / array.length;
